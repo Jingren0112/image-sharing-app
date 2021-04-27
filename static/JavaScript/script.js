c2f215f98c31882b371cca1418a5c7dcaa5fef6a
Jingren Li <lij9@tcd.ie>
2021-04-27 01:35:41 +0100

    update script for ejs
    modify send function to send selected images to server and reload current page to display success/error message
    create loadImage function for future use.

--- static/JavaScript/script.js
+++ static/JavaScript/script.js
@@ -7,41 +7,56 @@ function imageUpload(e){
             document.getElementById("preview").appendChild(document.createElement("br"));
         }
         setReader(e.target.files[i]);
+        imageName[i] = e.target.files[i].name;
         var img=document.createElement("IMG");
         img.src=URL.createObjectURL(e.target.files[i]);
         img.classList.add("col-3");
         document.getElementById("preview").appendChild(img);
     }
+ 
 }
 
-function onload(){
+function loadImages(){
     $.ajax({
         url:"/getImages",
         type:"POST",
         data: null
     }).done(function(result){
-        for(var i=0; i<result.length;i++){
-
+        if(result!=null){
+            for(var i=0; i<result.length; i++){
+                if(i%4==0&&i!=0){
+                    document.getElementById("imageset").appendChild(document.createElement("br"));
+                    document.getElementById("imageset").appendChild(document.createElement("br"));
+                }
+                var username = result[i].username;
+                var imageBlock = document.createElement("DIV");
+                imageBlock.classList.add("col-3");
+                var img=document.createElement("IMG");
+                var date = document.createElement("a");
+                var username = document.createElement("a");
+                img.src=result[i].src;
+                date.innerHTML= result[i].date;
+                username.innerHTML=result[i].username;
+                imageBlock.appendChild(username);
+                imageBlock.appendChild(date);
+                imageBlock.appendChild(img);
+                document.getElementById("imageset").appendChild(imageBlock);
+            }
         }
     });
 }
 
 function send(){
+    console.log(dataTrans);
+    var data ={"data": JSON.stringify(dataTrans),"imageName":JSON.stringify(imageName)};
     $.ajax({
         url:"/upload",
         type: "POST",
-        data: {"data":JSON.stringify(dataTrans)}
+        data: data
     }).done(function(result){
-        result=JSON.parse(result);
-        for(var i=0; i<result.length;i++){
-            var img=document.createElement("IMG");
-            img.src=result[i];
-            img.classList.add("col-3");
-            document.getElementById("preview").appendChild(img);
-        }
+            window.location.reload();
     });
 }
-
 function setReader(file){
     const reader = new FileReader();
     reader.onload=function(){
@@ -52,9 +67,7 @@ function setReader(file){
 }
 
 
-    
-
-var commentButton = document.createElement("a");
+var imageName = [];
 var data=[];
 var dataTrans=[];
 var j=0;
