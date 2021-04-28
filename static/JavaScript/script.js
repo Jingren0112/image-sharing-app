3c100f9b0b3d69012ce664ccb7eebd2516cd0b44
Jingren Li <lij9@tcd.ie>
2021-04-28 23:30:35 +0100

    Update script
    Allow user to delete the image they don't want to upload by clicking delete image button. also added comment.

--- static/JavaScript/script.js
+++ static/JavaScript/script.js
@@ -1,92 +1,117 @@
+
+
 /*load preview */
-function imageUpload(e){
+function imageUpload(e){                                        //preview image but reject the image that exceed 1MB to avoid connection issue
     var imageLength = e.target.files.length;
     for(var i=0; i<imageLength; i++){
-        if(i%4==0&&i!=0){
-            document.getElementById("preview").appendChild(document.createElement("br"));
-            document.getElementById("preview").appendChild(document.createElement("br"));
+        if(e.target.files[i].size<1000000){                     //bool for check file size
+            if(x%3==0&&x!=0){
+                document.getElementById("preview").appendChild(document.createElement("br"));
+                document.getElementById("preview").appendChild(document.createElement("br"));
+            }
+            setReader(e.target.files[i]);                           //convert images to base64 code and store in array
+            imageName[x] = e.target.files[i].name;                  // store corresponding name to array
+            var img=document.createElement("IMG");
+            var deleteImages = document.createElement("BUTTON");
+            deleteImages.className="btn btn-sm bgColor";
+            deleteImages.innerHTML="delete image";
+            deleteImages.type="button";
+            deleteImages.addEventListener('click',function(){
+                deleteIMG(this.id);
+            });
+            deleteImages.id=x;                                              //sync button id with array index
+            img.src=URL.createObjectURL(e.target.files[i]);
+            img.classList.add("col-2");
+            img.classList.add(x);                                               //add class name of current image index for delete
+            document.getElementById("preview").appendChild(img);
+            document.getElementById("preview").appendChild(deleteImages);
+            x++;
+        }else{
+            var message=document.createElement("p");
+            message.innerHTML="The file that exceed 1MB will not be upload.";
+            document.getElementById("errorMessage").appendChild(message);
         }
-        setReader(e.target.files[i]);
-        imageName[i] = e.target.files[i].name;
-        var img=document.createElement("IMG");
-        var deleteImages = document.createElement("BUTTON");
-        deleteImages.classList.add("btn");
-        deleteImages.classList.add("btn-sm");
-        deleteImages.classList.add("bgColor");
-        img.src=URL.createObjectURL(e.target.files[i]);
-        img.classList.add("col-3");
-        document.getElementById("preview").appendChild(img);
     }
  
 }
 
 function like(){
-    var idimages = document.getElementsByClassName("image")[0].id;
-    var dataToTransfer = {"idimages":idimages};
+    var idimages = document.getElementsByClassName("image")[0].id;              //use tag's id as the image id which can be used to search databse
+    var dataToTransfer = {"idimages":idimages};                                 //set data to transfer to server.
     $.ajax({
         url:"/like",
         type:"POST",
         data: dataToTransfer
-    }).done(function(result){
+    }).done(function(result){                                                   //after server finished, do things. I didn't use success/faild because i want to customize it later.
         if(result=="failed"){
-            window.location.href='/login';
+            window.location.href='/login';                                      //not login will redirect to login page.
         }else{
-            document.getElementById("likebtn").disabled = true;
+            document.getElementById("likebtn").disabled = true;                 // disable comment button after the user already commented.
             var currentValue=document.getElementById("count").innerHTML;
-            console.log(currentValue);
-            document.getElementById("count").innerHTML = parseInt(currentValue,10)+1;
+            document.getElementById("count").innerHTML = parseInt(currentValue,10)+1;       //add add the number of like by 1
         }
     });
 }
 
-function comment(){
-    var data=document.getElementById("commentInput");
-    var idimages = document.getElementsByClassName("image")[0].id;
-    var dataToTransfer={"data":data.value,"idimages":idimages};
+function comment(){                                                                 //upload comment to server
+    var data=document.getElementById("commentInput");                               //get comment data
+    var idimages = document.getElementsByClassName("image")[0].id;                  //get image id
+    var dataToTransfer={"data":data.value,"idimages":idimages};                     //create data to be transfer in json type
     $.ajax({
         url:"/comment",
         type:"POST",
         data: dataToTransfer
     }).done(function(result){
-        if(result=="failed"){
+        if(result=="failed"){                                                       //if not login then user will be redirect to login
             window.location.href='/login';
-        }else{
-            var comment=document.createElement("div");
-            comment.classList.add("row");
-            comment.classList.add("justify-content-center");
-            var commentData=document.createElement("p");
-            commentData.innerHTML="Comment: "+result.comment;
-            comment.appendChild(commentData);
-            var commentUser=document.createElement("div");
-            commentUser.classList.add("row");
-            commentUser.classList.add("justify-content-center");
-            var commentUsername=document.createElement("p");
-            commentUsername.innerHTML="From "+result.username;
-            commentUser.appendChild(commentUsername);
+        }else{                                                                      //post the new comment to the comment area
+            var commentBox=document.createElement("div");              
+            commentBox.className="d-flex justify-content-center py-2";
+            var commentBox2 = document.createElement("div");
+            commentBox2.className="second py-2 px-2";
+            var comment = document.createElement("span");
+            comment.className="text1";
+            comment.innerHTML=result.comment;
+            var commentUserAndDate = document.createElement("div");            
+            commentUserAndDate.className="d-flex justify-content-between py-1 pt-2";
+            var userDiv = document.createElement("div");
+            var user = document.createElement("span")
+            user.className="text2";
+            user.innerHTML=result.username;
+            var dateDiv = document.createElement("div");
+            var date = document.createElement("span");
+            date.className="text3";
+            date.innerHTML=result.date;
+            dateDiv.appendChild(date);
+            userDiv.appendChild(user);
+            commentUserAndDate.appendChild(userDiv);
+            commentUserAndDate.appendChild(dateDiv);
+            commentBox2.appendChild(comment);
+            commentBox2.appendChild(commentUserAndDate);
+            commentBox.appendChild(commentBox2);
             var area = document.getElementById("commentArea");
-            if(document.getElementById("nocomment")!=null){
+            if(document.getElementById("nocomment")!=null){             //check if there's a nocomment string.
                 area.innerHTML="";
             }
-            area.appendChild(comment);
-            area.appendChild(commentUser);
+            area.appendChild(commentBox);
         }
     });
 }
 
 
-function send(){
-    var data ={"data": JSON.stringify(dataTrans),"imageName":JSON.stringify(imageName)};
+function send(){                            //upload image to server
+    var data ={"data": JSON.stringify(dataTrans),"imageName":JSON.stringify(imageName)};       //use stringfy to compress all data into a single json object fo transfer
     $.ajax({
         url:"/upload",
         type: "POST",
         data: data
     }).done(function(){
-            window.location.reload();
+            window.location.reload();                            //reload the page to display the result using flash module.
     });
 }
 
 
-function setReader(file){
+function setReader(file){                                           //file reader function to read the image as data url
     const reader = new FileReader();
     reader.onload=function(){
         dataTrans[j]=reader.result;
@@ -96,11 +121,18 @@ function setReader(file){
 }
 
 
-function deleteImages(e){
+function deleteIMG(btnID){                                          //delete unwanted image
+    var image = document.getElementsByClassName(btnID)[0];
+    var button = document.getElementById(btnID);
+    image.remove();
+    button.remove();
+    imageName[btnID]="";
+    dataTrans[btnID]="";
 
 }
 
-var imageName = [];
-var data=[];
-var dataTrans=[];
-var j=0;
+var imageName = [];                 //user uploaded image name array
+var data=[];                        //user uploaded image data array
+var dataTrans=[];                   //user uploaded image data in url form array
+var j=0;                            //index for the data arrays above
+var x=0;                            //index for other array
